ac80aec22aa44fe0f72ea039d53d7168b0978806
pupupeter <126063403+pupupeter@users.noreply.github.com>
2025-03-04 11:21:26 +0800

    Update smoleagent.py

--- smoleagent.py
+++ smoleagent.py
@@ -2,7 +2,7 @@ from smolagents import CodeAgent, HfApiModel
 
 model_id = "Qwen/Qwen2.5-Coder-32B-Instruct"
 
-model = HfApiModel(model_id=model_id, token="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")
+model = HfApiModel(model_id=model_id, token="your huggingface  token")
 agent = CodeAgent(tools=[], model=model, add_base_tools=True)
 
 agent.run(
